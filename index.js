df1d493b509dd38cceea345af4c161ea7af045e7
KunalShaurya <kunal.shaurya@tagbin.in>
2024-10-07 14:29:25 +0530

    updated-vercel

---
+++ index.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import app from "./src/app.js";
+
+dotenv.config(); 
+
+const PORT = process.env.PORT || 8001;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
+});
