17264fcadb1f73ef70ca0a6e232ca71f563f3dab
KunalShaurya <kunal.shaurya@tagbin.in>
2024-10-10 15:33:04 +0530

    modelView-setup-folder

---
+++ models/users.js
@@ -0,0 +1,50 @@
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+
+    lastName: {
+      type: String,
+    },
+    password: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    gender: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt();
+    user.password = await bcrypt.hash(user.password, salt);
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+
+module.exports = User;
